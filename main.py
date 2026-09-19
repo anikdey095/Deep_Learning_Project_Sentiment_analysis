ed0979612a861781589cc423b8942f3d6784136d
anikdey095 <anikd1452@gmail.com>
2026-09-19 15:33:51 +0600

    works on fastapi

--- main.py
+++ main.py
@@ -1,3 +1,105 @@
-from fastapi import FASTAPI, FastAPI
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from fastapi.responses import FileResponse
+from pydantic import BaseModel, Field
+from keras.models import load_model
+import numpy as np
+import pickle
+import re
+from fastapi import FastAPI
 
 app=FastAPI()
+@app.get("/")
+def greet():
+    return "Hello, World!"
+
+"""
+1. We are going to make some constants like:
+A. Model Path (BiGRU)
+B. Tokenizer Path
+C. Max Sequence Length
+D. Emotion Labels
+E. Emotion emojis
+"""
+
+model_path="Artifacts/BiGRU_Modle.keras"
+tokenizer_path="Artifacts/tokenizer.pkl"
+
+max_sequence_length=50
+
+#D. Emotion Labels
+emotion_labels = ["sadness", "joy", "love", "anger", "fear", "surprise"]
+
+#E. Emotion emojis
+EMOTION_EMOJIS = {
+    "sadness": "😢",
+    "joy": "😄",
+    "love": "❤️",
+    "anger": "😠",
+    "fear": "😨",
+    "surprise": "😲",
+}
+"""
+2. Preprocess the upcoming text
+Cleans raw text so it matches the format used while training.
+A. Convert the text to lowercase. -done
+B. Remove apostrophes (e.g can't -> cant). -done
+C. Remove Special Characters and Punctuation. -done
+D. Remove extra spaces -done
+"""
+def preprocess_text(text: str)->str:
+    text = text.lower()
+    text = re.sub(r"'","",text)
+    text = re.sub(r"[^a-z0-9\s]"," ", text)
+    text = re.sub(r"\s+", " ",text).strip()
+    return text
+
+"""
+3. Request and Response Schemas
+A. Text Input -> Input schema the text sent by user. -done
+B. Prediciton Response -> Output schema the emotion to predict. -done
+C. Health Response (Server health check)
+"""
+
+class TextInput(BaseModel):
+    text : str = Field(
+        ...,
+        min_length=1,
+        max_length=2000,
+        description="The sentence to analyze",
+        json_schema_extra={"example": "I feel so happy and excited"}
+        )
+
+class PredictionResponse(BaseModel):
+    text: str
+    predicted_emotion: str
+    confidence : float
+    all_probabilites: dict[str, float]
+
+class HealthResponse(BaseModel):
+    status: str
+    model_loaded: bool
+
+    
+"""
+4. Model Loading and LifeSpan Management
+Load the model and toknizer once the server starts up.
+"""
+dl_model = {} #{1. BiGRU, 2. Tokenizer}-> True , {} -> False
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print('Loading the model and tokenizer...')
+    dl_model["BiGRU"] = load_model(model_path)                      #BiGRU Model
+    with open(tokenizer_path, 'rb') as file:
+        dl_model["Tokenizer"] = pickle.load(file)
+    print('Model are loaded successfully...')   
+
+    yield #Pause, model is laoded and server is running and at this point model wait karega for request
+
+    dl_model.clear() #Ek baar server band ho gaya uske baad model ko memory se hata do.
+               
